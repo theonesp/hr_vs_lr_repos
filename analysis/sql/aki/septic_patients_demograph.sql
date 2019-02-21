d4a72c6e73e99402040d112f5dfc81ca8c523891
theonesp <ma.armengol.hcsc@gmail.com>
2019-02-21 16:33:35 -0500

    Create septic_patients_demograph.sql

---
+++ analysis/sql/aki/septic_patients_demograph.sql
@@ -0,0 +1,57 @@
+WITH sq AS (SELECT
+ p.uniquepid,
+ p.patienthealthsystemstayid,
+ p.patientunitstayid,
+ ROW_NUMBER() OVER (PARTITION BY p.uniquepid ORDER BY p.patientunitstayid ASC) AS position
+  , p.gender
+  , case -- fixing age >89 to 93
+                WHEN p.age LIKE '%89%' then 93 -- age avg of patients >89
+                ELSE p.age::integer end AS age_fixed
+  , p.admissionheight AS height
+  , p.admissionweight AS weight
+  , p.ethnicity
+  , p.hospitaldischargeyear
+  , p.hospitaladmitsource
+  , p.unittype
+  , p.unitadmitsource
+  , p.unitdischargetime24
+  , p.apacheadmissiondx
+  , a.actualicumortality
+  , a.actualhospitalmortality
+  , p.hospitalid
+  , s.readmit
+  , h.numbedscategory
+  , h.teachingstatus
+  , h.region
+  , a.unabridgedunitlos
+  , a.unabridgedhosplos
+  , a.unabridgedactualventdays
+  , t.intubated AS intubated_first_24h
+  , s.aids
+  , s.hepaticfailure
+  , s.lymphoma
+  , s.metastaticcancer
+  , s.leukemia
+  , s.immunosuppression
+  , s.cirrhosis
+  , s.diabetes
+  , s.electivesurgery
+  , t.dialysis AS chronic_dialysis_prior_to_hospital
+  , s.activetx
+  , a.apachescore
+FROM eicu_crd_v2.patient p
+LEFT JOIN eicu_crd_v2.apachepredvar s
+  ON  p.patientunitstayid =s.patientunitstayid
+LEFT JOIN eicu_crd_v2.apachepatientresult a
+  ON p.patientunitstayid = a.patientunitstayid
+LEFT JOIN eicu_crd_v2.apacheapsvar t
+  ON p.patientunitstayid = t.patientunitstayid
+LEFT JOIN eicu_crd_v2.hospital h
+  ON  p.hospitalid = h.hospitalid
+WHERE p.apacheadmissiondx ILIKE '%sepsis%'
+  AND s.readmit = 0
+  AND p.age NOT IN ( '0', '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15')
+  AND a.actualhospitalmortality IS NOT NULL
+  AND hospitaldischargeyear = 2014
+  order by p.uniquepid )
+ SELECT * FROM sq WHERE position = 1 --first ICU admission
